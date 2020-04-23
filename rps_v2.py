eaf2137533bf65df6dfe362c1355c4189289a7c1
Clifford Butler <cliff002@msn.com>
2020-04-23 13:27:58 -0700

    Update rps_v2.py
    Communication string added.

--- rps_v2.py
+++ rps_v2.py
@@ -1,2 +1,9 @@
 # import random module 
 import random 
+
+# Print multiline instruction 
+# performstring concatenation of string 
+print("Winning Rules of the Rock paper scissor game as follows: \n"
+                                +"Rock vs paper->paper wins \n"
+                                + "Rock vs scissor->Rock wins \n"
+                                +"paper vs scissor->scissor wins \n") 
